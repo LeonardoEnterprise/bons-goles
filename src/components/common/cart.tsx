91c9d1e32798ba1854f887c59311a724c651628d
LeonardoPaniz <102881927+LeonardoPaniz@users.noreply.github.com>
2025-08-17 18:38:24 -0300

    feat: add cart items

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -12,6 +12,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import CartItem from "./cart-item";
 
 const Cart = () => {
   const { data: cart, isPending } = useQuery({
@@ -29,22 +30,18 @@ const Cart = () => {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div>
+        <div className="space-y-4 px-5">
           {isPending && <p>Carregando...</p>}
           {cart?.items?.map((item) => (
-            <div key={item.id}>
-              <Image
-                src={item.productVariant.imageUrl}
-                alt={item.productVariant.name}
-                width={100}
-                height={100}
-              />
-              <div>
-                <h3>{item.productVariant.product.name}</h3>
-                <p>{item.productVariant.name}</p>
-                <p>{item.quantity}</p>
-              </div>
-            </div>
+            <CartItem
+              key={item.id}
+              id={item.id}
+              productName={item.productVariant.product.name}
+              productVariantName={item.productVariant.name}
+              productVariantImageUrl={item.productVariant.imageUrl}
+              productVariantPriceInCents={item.productVariant.priceInCents}
+              quantity={item.quantity}
+            />
           ))}
         </div>
       </SheetContent>
