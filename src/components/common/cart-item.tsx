984ef5f66d3739d6dbc57e39e9cfdee56ee621a7
LeonardoPaniz <102881927+LeonardoPaniz@users.noreply.github.com>
2025-08-17 20:08:27 -0300

    feat: ensure cart product quantity can be decreased

--- src/components/common/cart-item.tsx
+++ src/components/common/cart-item.tsx
@@ -3,6 +3,7 @@ import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { toast } from "sonner";
 
+import { decreaseCartProductQuantity } from "@/actions/decrease-cart-product-quantity";
 import { removeProductFromCart } from "@/actions/remove-cart-products";
 import { formatCentsToBRL } from "@/helpers/money";
 
@@ -26,6 +27,7 @@ const CartItem = ({
   quantity,
 }: CartItemProps) => {
   const queryClient = useQueryClient();
+
   const removeProductFromCartMutation = useMutation({
     mutationKey: ["remove-to-cart"],
     mutationFn: () => removeProductFromCart({ cartItemId: id }),
@@ -33,6 +35,14 @@ const CartItem = ({
       queryClient.invalidateQueries({ queryKey: ["cart"] });
     },
   });
+  const decreaseCartProductQuantityMutation = useMutation({
+    mutationKey: ["decrease-cart-product-quantity"],
+    mutationFn: () => decreaseCartProductQuantity({ cartItemId: id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    },
+  });
+
   const handleDeleteClick = () => {
     removeProductFromCartMutation.mutate(undefined, {
       onSuccess: () => {
@@ -40,6 +50,17 @@ const CartItem = ({
       },
       onError: () => {
         toast.error("Erro ao remover produto do carrinho.");
+      },
+    });
+  };
+
+  const handleDecreaseQuantityClick = () => {
+    decreaseCartProductQuantityMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Quantidade do produto atualizada.");
+      },
+      onError: () => {
+        toast.error("Erro ao atualizar quantidade do produto.");
       },
     });
   };
@@ -61,7 +82,11 @@ const CartItem = ({
             {productVariantName}
           </p>
           <div className="flex w-[100px] items-center justify-between rounded-lg border p-1">
-            <Button className="h-4 w-4" variant="ghost" onClick={() => {}}>
+            <Button
+              className="h-4 w-4"
+              variant="ghost"
+              onClick={handleDecreaseQuantityClick}
+            >
               <MinusIcon />
             </Button>
             <p className="text-xs font-medium">{quantity}</p>
@@ -72,11 +97,7 @@ const CartItem = ({
         </div>
       </div>
       <div className="flex flex-col items-end justify-center gap-2">
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => handleDeleteClick()}
-        >
+        <Button variant="outline" size="icon" onClick={handleDeleteClick}>
           <TrashIcon />
         </Button>
         <p className="text-sm font-bold">
